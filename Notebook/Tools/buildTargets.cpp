37aeede6ec0e1b89fdedd57b24ff41f0bf1cc30b
Seth Hendrick <srh7240@rit.edu>
2013-01-16 23:30:40 -0500

    Added Lenny's Book Class and made an Auto Builder
    An Autobuilder now lives inside of the tools folder.  This will
    automatically build targets with the aid of code::blocks.

---
+++ Notebook/Tools/buildTargets.cpp
@@ -0,0 +1,64 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int numFailed = 0;
+	int mainRet;
+	cout<<"Building Projects"<<std::endl;
+	
+	ifstream inFile;
+	
+	inFile.open("projects.txt");
+	
+	inFile.peek();
+	
+	string path;
+	string fileName;
+	string target;
+	
+	while (!inFile.eof() && !inFile.fail()){
+		inFile >> path;
+		inFile >> fileName;
+		inFile >> target;
+		
+		stringstream ss;
+		
+		ss << "codeblocks.exe  --rebuild --target=\"" << target << "\" " << path << "\\" << fileName;
+		
+		cout<<ss.str() <<endl;
+		
+		int ret = system(ss.str().c_str());
+		
+		if (ret != 0){
+			++numFailed;
+		}
+		
+		inFile.peek();
+	}
+	
+	if (inFile.fail() && !inFile.eof()){
+		cout<<"File could not be opened"<<endl;
+		system("pause");
+		return -1;
+	}
+	else{
+		inFile.close();
+	}
+	if (numFailed != 0){
+		cout << "WARNING! "<<numFailed<<" Files does not build"<<endl;
+		mainRet = numFailed; 
+	}
+	else{
+		cout<<"All targets built!"<<endl;
+		mainRet=0;
+	}	
+	system("pause");
+	
+	return mainRet;
+}
