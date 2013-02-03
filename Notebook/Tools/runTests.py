34607523651d777bf88461e97052040131cc46db
Seth Hendrick <srh7240@rit.edu>
2013-02-03 01:53:26 -0500

    Added a test Runner in Python 1. Changed the tool's runTests.bat from a batch file to a python file 2. Did the same for container's runTests.bat 3. Removed core Tests from tests.txt as it is current Broken

---
+++ Notebook/Tools/runTests.py
@@ -0,0 +1,19 @@
+#runTests.py
+#Description: Runs all of the tests located in 
+
+import sys
+import os
+
+absPath = os.getcwd()
+headerPath = os.path.join(absPath, "src")
+sys.path.append(headerPath)
+import runTestHeader
+
+numFailed = runTestHeader.runTests(absPath, "tests.txt")
+
+if (numFailed == 0):
+    print("All Tests Pass!")
+else:
+    print(numFailed + " Tests Failed!")
+
+input("Press Any Key to continue")
