4632b402986b3ca2b0545d089356764124592c9f
Seth Hendrick <srh7240@rit.edu>
2013-04-15 21:14:47 -0400

    Added a way to build all libs right in the core directory.

---
+++ Notebook/buildLibs.py
@@ -0,0 +1,28 @@
+#Builds all the libraries
+
+import os
+import subprocess
+
+#Add to here the directories that contain libs that need to be built
+libDirs = [os.path.join("Common", "os"), "Core", "SkyWrite-Core"]
+
+thisDir = os.getcwd()
+
+errors = 0
+
+for dir in libDirs:
+    os.chdir(dir)
+    cleanLib = "scons create_lib --clean"
+    print (cleanLib)
+    subprocess.call(cleanLib, shell=True)
+    
+    createLib = "scons create_lib"
+    print (createLib)
+    status = subprocess.call(createLib, shell=True)
+    if (status != 0):
+        errors = errors + 1
+
+    os.chdir(thisDir)
+        
+if (errors != 0):
+    print ("***Errors have occured!***")
