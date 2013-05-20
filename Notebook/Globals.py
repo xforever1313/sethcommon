0f126718e4d2d6576ba7ecd21da4a9d0299f864d
Seth Hendrick <srh7240@rit.edu>
2013-05-19 22:38:57 -0400

    Added the start of a CLI for Notebook 1.  Added a new Notebook-CLI project for testing purposes 2.  Fixed a memory leak in NotebookAPI

--- Notebook/Globals.py
+++ Notebook/Globals.py
@@ -21,17 +21,17 @@ debugDir = "debug"
 releaseDir = "release"
 unitTestDir = "unit_test"
 
+def getCommonClassesPath(env):
+    return os.path.join(getCommonPath(env), "common_classes")
+
 def getCommonPath(env):
     return os.path.join(env['NOTEBOOK_ROOT'], "Common")
 
-def getCorePath(env):
-    return os.path.join(env['NOTEBOOK_ROOT'], "Core")
-
+def getNotebookCorePath(env):
+    return os.path.join(env['NOTEBOOK_ROOT'], "Notebook-Core")
+    
 def getSCSVPath(env):
     return os.path.join(getCommonPath(env), "SCSV")
     
 def getSkyvoOSPath(env):
     return os.path.join(getCommonPath(env), "os")
-
-def getCommonClassesPath(env):
-    return os.path.join(getCommonPath(env), "common_classes")
