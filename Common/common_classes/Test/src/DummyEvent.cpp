6a078927aa6ccdf45a818132465d1323cced8aa6
Seth Hendrick <xforever1313@aim.com>
2014-02-27 21:01:30 -0500

    Added licensing information to common_classes.

--- Common/common_classes/Test/src/DummyEvent.cpp
+++ Common/common_classes/Test/src/DummyEvent.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright Seth Hendrick 2014
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef ASM_JS //Only used in exectutor test, which is not usable with emscripten
 #include <SkyvoThread.h>
 
