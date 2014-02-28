a6b88a28072673885921c005c7ce4dad2370243b
Seth Hendrick <xforever1313@aim.com>
2014-02-27 22:46:47 -0500

    Added licensing information for SCSV Builds.

--- Common/SCSV/include/SCSVException.h
+++ Common/SCSV/include/SCSVException.h
@@ -1,3 +1,9 @@
+
+//          Copyright Seth Hendrick 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef SCSV_EXCEPTION_H_
 #define SCSV_EXCEPTION_H_
 
