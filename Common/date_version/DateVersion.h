ecfbdc9eb03c63ac34e7c0e8f1642d35e312a6d7
Seth Hendrick <xforever1313@aim.com>
2014-02-27 22:23:51 -0500

    Boost License added to dateVersion.

--- Common/date_version/DateVersion.h
+++ Common/date_version/DateVersion.h
@@ -1,3 +1,10 @@
+
+//          Copyright Seth Hendrick 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
 //Include guards are missing on purpose.  The purpose of this file is to be
 //#included in to several namespaces.  Therefore, no include guards needed.
 
