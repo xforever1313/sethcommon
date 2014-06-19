50469aceca9b29e1bb38d90f0b9b2196e49b5a7f
Seth Hendrick <xforever1313@aim.com>
2014-06-18 22:07:42 -0400

    Added licensing to the IO files in common_classes.

--- Common/common_classes/include/io/LoggerBase.h
+++ Common/common_classes/include/io/LoggerBase.h
@@ -1,3 +1,9 @@
+
+//          Copyright Seth Hendrick 2014
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef LOGGER_BASE_H_
 #define LOGGER_BASE_H_
 
--- Common/common_classes/src/io/InputReader.cpp
+++ Common/common_classes/src/io/InputReader.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright Seth Hendrick 2014
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include "io/ConsoleLogger.h"
 #include "io/InputReader.h"
 #include "io/LoggerBase.h"
